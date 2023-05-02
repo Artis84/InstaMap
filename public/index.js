3105033f97c4e53c48696b3e31847b952ad67651
Artis84 <lopsta10@gmail.com>
2023-05-03 01:48:09 +0200

    add click effect,text status and some css shit

--- public/index.js
+++ public/index.js
@@ -8,62 +8,85 @@ L.tileLayer("https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
 }).addTo(myMap);
 
 // Add a click event listener to the map
-myMap.on("click", function (e) {
+myMap.on("click", async (e) => {
     // Get the coordinates of the clicked location
     const lat = e.latlng.lat;
     const lng = e.latlng.lng;
 
-    // Search for YouTube videos related to the city name
-    const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&location=${lat},${lng}&locationRadius=1m&key=AIzaSyB5E4zHuceMvKNj7VHkPRyH-iCh9DGiuuM`;
-    fetch(youtubeUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            // Get the video data from the YouTube API response
-            const videos = data.items.map((item) => {
-                return {
-                    title: item.snippet.title,
-                    thumbnail: item.snippet.thumbnails.medium.url,
-                    videoId: item.id.videoId,
-                };
-            });
+    // show the click effect
+    const clickCircle = document.getElementById("click-circle");
+    const circleSize = 50; // adjust as necessary
+    clickCircle.style.top = e.originalEvent.clientY - circleSize / 2 + "px";
+    clickCircle.style.left = e.originalEvent.clientX - circleSize / 2 + "px";
+    clickCircle.style.display = "block";
+    setTimeout(function () {
+        clickCircle.style.display = "none";
+    }, 500);
 
-            // Remove the previous video container if it exists
-            const sideMenuNode = document.getElementById("side-menu") || undefined;
-            if (sideMenuNode) {
-                sideMenuNode.remove();
-            }
+    const statusNode = document.getElementById("status");
+    const sideMenuNode = document.getElementById("side-menu") || undefined;
+    try {
+        // Search for YouTube videos related to the city name
+        const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&location=${lat},${lng}&locationRadius=1m&key=AIzaSyD1c4bzFTJ_gjUjUydPL3gGtgJ91CL5NtA`;
 
-            // Create a side menu with the videos
-            const sideMenu = document.createElement("ul");
-            sideMenu.id = "side-menu";
-            videos.forEach((video) => {
-                videoContainer = document.createElement("li");
-                videoContainer.classList.add("video-container");
+        const response = await fetch(youtubeUrl);
+        const data = await response.json();
 
-                const thumbnail = document.createElement("img");
-                thumbnail.src = video.thumbnail;
-                thumbnail.classList.add("thumbnail");
-                videoContainer.appendChild(thumbnail);
+        if (Object.keys(data.items).length == 0) {
+            throw "No results found";
+        }
 
-                const title = document.createElement("div");
-                title.textContent = decodeURIComponent(video.title);
-                title.classList.add("title");
-                videoContainer.appendChild(title);
-
-                videoContainer.addEventListener("click", function () {
-                    // Create a popup and display the YouTube video
-                    const popupContent =
-                        '<iframe src="https://www.youtube.com/embed/' +
-                        video.videoId +
-                        '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-                    L.popup().setLatLng(e.latlng).setContent(popupContent).openOn(myMap);
-                });
-
-                sideMenu.appendChild(videoContainer);
-            });
-
-            // Add the side menu to the page
-            const menu = document.getElementById("menu");
-            menu.appendChild(sideMenu);
+        if (statusNode.textContent) {
+            statusNode.style.display = "none";
+        }
+        // if (!data.length) throw new Error("No results found");
+        const videos = data.items.map((item) => {
+            return {
+                title: item.snippet.title,
+                thumbnail: item.snippet.thumbnails.medium.url,
+                videoId: item.id.videoId,
+            };
         });
+
+        // Remove the previous video container if it exists
+        if (sideMenuNode) {
+            sideMenuNode.remove();
+        }
+        // Create a side menu
+        const sideMenu = document.createElement("ul");
+        sideMenu.id = "side-menu";
+
+        videos.forEach((video) => {
+            videoContainer = document.createElement("li");
+            videoContainer.classList.add("video-container");
+
+            const thumbnail = document.createElement("img");
+            thumbnail.src = video.thumbnail;
+            thumbnail.classList.add("thumbnail");
+            videoContainer.appendChild(thumbnail);
+
+            const title = document.createElement("div");
+            title.textContent = video.title;
+            title.classList.add("title");
+            videoContainer.appendChild(title);
+
+            videoContainer.addEventListener("click", function () {
+                // Create a popup and display the YouTube video
+                const popupContent =
+                    '<iframe src="https://www.youtube.com/embed/' +
+                    video.videoId +
+                    '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+                L.popup().setLatLng(e.latlng).setContent(popupContent).openOn(myMap);
+            });
+
+            // Populate the side menu with videos
+            sideMenu.appendChild(videoContainer);
+        });
+        const menu = document.getElementById("menu");
+        menu.appendChild(sideMenu);
+    } catch (error) {
+        if (sideMenuNode) sideMenuNode.remove();
+        statusNode.style.display = "block";
+        error === "No results found" ? (statusNode.innerHTML = "No results found") : (statusNode.innerHTML = "An error occured 😟");
+    }
 });
