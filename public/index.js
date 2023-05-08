38316d6ff00f64b3ddd3ea7c122d1fbbff77798a
Artis84 <lopsta10@gmail.com>
2023-05-08 23:41:11 +0200

    add overlay and somme css shit

--- public/index.js
+++ public/index.js
@@ -25,34 +25,36 @@ myMap.on("click", async (e) => {
 
     const statusNode = document.getElementById("status");
     const sideMenuNode = document.getElementById("side-menu") || undefined;
+    const overlayNode = document.querySelector(".overlay") || undefined;
     try {
         // Search for YouTube videos related to the city name
-        const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&location=${lat},${lng}&locationRadius=1m&key=AIzaSyD1c4bzFTJ_gjUjUydPL3gGtgJ91CL5NtA`;
+        const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&location=${lat},${lng}&locationRadius=1m&key=AIzaSyD5WI9PN7w7s8lhVj1U5QBICulQxjiZK9U`;
 
         const response = await fetch(youtubeUrl);
         const data = await response.json();
 
-        if (Object.keys(data.items).length == 0) {
-            throw "No results found";
-        }
+        // Remove the previous video container if it exists
+        if (sideMenuNode) sideMenuNode.remove();
+        if (overlayNode) overlayNode.remove();
 
-        if (statusNode.textContent) {
-            statusNode.style.display = "none";
-        }
+        if (statusNode.textContent) statusNode.style.display = "none";
+
+        if (Object.keys(data.items).length == 0) throw "No results found";
+
         // if (!data.length) throw new Error("No results found");
         const videos = data.items.map((item) => {
+            const date = new Date(item.snippet.publishedAt);
+            const year = date.getFullYear();
             return {
-                title: item.snippet.title,
+                title: item.snippet.title + " (" + year + ")",
                 thumbnail: item.snippet.thumbnails.medium.url,
                 videoId: item.id.videoId,
             };
         });
 
-        // Remove the previous video container if it exists
-        if (sideMenuNode) {
-            sideMenuNode.remove();
-        }
         // Create a side menu
+        const overlay = document.createElement("div");
+        overlay.className = "overlay";
         const sideMenu = document.createElement("ul");
         sideMenu.id = "side-menu";
 
@@ -82,11 +84,22 @@ myMap.on("click", async (e) => {
             // Populate the side menu with videos
             sideMenu.appendChild(videoContainer);
         });
+
         const menu = document.getElementById("menu");
+        menu.style.display = "block";
         menu.appendChild(sideMenu);
+        const sideMenuHeight = document.getElementById("side-menu");
+
+        // const hiddenContentHeight = sideMenuHeight.scrollHeight - (sideMenuHeight.scrollHeight - menu.clientHeight);
+        if (sideMenuHeight.scrollHeight - 40 > menu.clientHeight) {
+            menu.appendChild(overlay);
+            console.log("Overflow detected! " + "\nscrollHeight: " + sideMenuHeight.scrollHeight + "\nclientHeight: " + menu.clientHeight);
+        } else {
+            console.log("No Overflow detected! " + "\nscrollHeight: " + sideMenuHeight.scrollHeight + "\nclientHeight: " + menu.clientHeight);
+        }
     } catch (error) {
-        if (sideMenuNode) sideMenuNode.remove();
         statusNode.style.display = "block";
         error === "No results found" ? (statusNode.innerHTML = "No results found") : (statusNode.innerHTML = "An error occured 😟");
+        console.error(error);
     }
 });
